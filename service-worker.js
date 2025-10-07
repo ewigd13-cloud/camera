60c1fb990c9cdacf04195d95f4f3c896435db578
ewigd13 <ewig.d13@gmail.com>
2025-10-07 12:46:06 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const urlsToCache = [
   self.location.origin + '/camera/',
   self.location.origin + '/camera/index.html',
   self.location.origin + '/camera/manifest.json',
-  self.location.origin + '/camera/assets/index--DJ-73WN.js', // ← dist/assets にある正確なファイル名
+  self.location.origin + '/camera/assets/index-Co7rOBCp.js', // ← dist/assets にある正確なファイル名
   self.location.origin + '/camera/assets/index-CTSoWR9A.css',
   'https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;700;900&display=swap'
 ];
